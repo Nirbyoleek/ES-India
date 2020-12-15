c6cfedd084b39562acd5b0f400c1b013ca08dda4
jugal09xx <65349424+jugal09xx@users.noreply.github.com>
2020-12-15 22:38:05 +0530

    add heading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Test from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to ES India's Landing page</h1>
+      <img src={Test}/>
     </div>
   );
 }
