ab3604f2c10a2a3dfe0cfe1fd07b7aaf44af5110
Samaresh Samanta <ss9261@srmist.edu.in>
2020-12-19 18:55:35 +0530

    build the desktop version

--- src/components/LandingPage/intro/intro.js
+++ src/components/LandingPage/intro/intro.js
@@ -4,46 +4,61 @@ import classes from "./introStyle.module.css";
 import "./intro.css";
 import Building from '../../../assets/BuildingForIntro.svg';
 import man from '../../../assets/Image2Intro.svg';
+import line from '../../../assets/line.svg';
+import Carousel from "./carousel";
 
 const Intro = () => {
     return(
         <div className={classes.main}>
             <Header />
-            <div className={classes.HeadingDesign}>
-                <p className={classes.frontText}>
-                    We are<br />EXPERT<br />AE SOLUTIONS                
-                </p>
-                <p className={classes.backgroundText}>
-                    We are<br />EXPERT<br />AE SOLUTIONS
-                </p>
-                <div className={classes.buttonContainer}>
-                    <a href="#abt" className="button">Know More</a>
+            <div className={classes.introPart1}>
+                <div className={classes.HeadingDesign}>
+                    <p className={classes.backgroundText}>
+                        We are<br />EXPERT<br />AE SOLUTIONS
+                    </p>
+                    <p className={classes.frontText}>
+                        We are<br />EXPERT<br />AE SOLUTIONS                
+                    </p>
+                    <div className={classes.buttonContainer}>
+                        <a href="#abt" className="button">Know More</a>
+                    </div>
+                </div>
+                <div className={classes.buildingImageContainer}>
+                    <img src={Building}/>
                 </div>
             </div>
-            <div className={classes.buildingImageContainer}>
-                <img src={Building}/>
+            <div className={classes.introPart2}>
+                <div className={classes.workIntro}>
+                    <p>
+                        The best AE consultancy services!
+                        With a track record of satisfied clients 
+                        and on-time work completion, you would 
+                        definitely want us to be your consultant.
+                    </p>
+                    <br />
+                    <p>
+                        The firm has continued to imbibe the vision 
+                        of “Safe, Durable and Economical Structures” 
+                        from its ambitious, industrious and tenacious 
+                        partners. At present, the firm functions with 
+                        multi-discipline and experienced experts, well 
+                        qualified staff and other professionals.
+                    </p>
+                </div>
+                <div className={classes.ManImageContainer}>
+                    <img src={man}/>
+                    <div></div>
+                </div>
             </div>
-            <div className={classes.workIntro}>
-                <p>
-                    The best AE consultancy services!
-                    With a track record of satisfied clients 
-                    and on-time work completion, you would 
-                    definitely want us to be your consultant.
-                </p>
-                <br />
-                <p>
-                    The firm has continued to imbibe the vision 
-                    of “Safe, Durable and Economical Structures” 
-                    from its ambitious, industrious and tenacious 
-                    partners. At present, the firm functions with 
-                    multi-discipline and experienced experts, well 
-                    qualified staff and other professionals.
-                </p>
+            <div className={classes.introPart3}>
+                <div className={classes.carousel}>
+                    <Carousel />
+                </div>
+                <div className={classes.introLastText}>
+                    <p>We Design For You</p>    
+                    <div></div>            
+                </div>
             </div>
-            <div className={classes.ManImageContainer}>
-                <img src={man}/>
-            </div>
-            <div></div>
         </div>
     )
 };
--- src/components/nav-header/nav-header.js
+++ src/components/nav-header/nav-header.js
@@ -11,6 +11,13 @@ const Header = () => {
             </div>
             <div className={classes.navMenuContainer}>
                 <img src={Bar} />
+                <ul>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Specialisations</a></li>
+                    <li><a href="#">Works</a></li>
+                    <li><a href="#">Our team</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
             </div>
         </div>
     )
