358d2ba7eb04b439b83170bc713400a16e74e9d9
Nirbyoleek <nirbyoleek152001@gmail.com>
2020-12-20 20:44:21 +0530

    Added client section

--- src/components/LandingPage/OurClients/OurClients.js
+++ src/components/LandingPage/OurClients/OurClients.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./OurClients.css";
+import classes from "./OurClients.module.css";
 import client1 from "../../../assets/client1.svg";
 import client2 from "../../../assets/client2.svg";
 import client3 from "../../../assets/client3.svg";
@@ -7,8 +7,8 @@ import client3 from "../../../assets/client3.svg";
 const OurClients = () => {
   return (
     <div>
+      <h2>Our Clients</h2>
       <div className={classes.Container}>
-        <h2>Our Clients</h2>
         <div className={classes.Section}>
           <div className={classes.Content}>
             <img src={client1} alt="" />
@@ -19,7 +19,29 @@ const OurClients = () => {
           <div className={classes.Content}>
             <img src={client3} alt="" />
           </div>
-        </div>
+        </div>{" "}
+        <div className={classes.Section}>
+          <div className={classes.Content}>
+            <img src={client1} alt="" />
+          </div>{" "}
+          <div className={classes.Content}>
+            <img src={client2} alt="" />
+          </div>{" "}
+          <div className={classes.Content}>
+            <img src={client3} alt="" />
+          </div>
+        </div>{" "}
+        <div className={classes.Section}>
+          <div className={classes.Content}>
+            <img src={client1} alt="" />
+          </div>{" "}
+          <div className={classes.Content}>
+            <img src={client2} alt="" />
+          </div>{" "}
+          <div className={classes.Content}>
+            <img src={client3} alt="" />
+          </div>
+        </div>{" "}
       </div>
     </div>
   );
