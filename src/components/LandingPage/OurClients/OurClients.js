917de9c5d8205cbf947714030b176fbc254d0e49
Nirbyoleek <nirbyoleek152001@gmail.com>
2020-12-20 20:15:52 +0530

    added client section

---
+++ src/components/LandingPage/OurClients/OurClients.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./OurClients.css";
+import client1 from "../../../assets/client1.svg";
+import client2 from "../../../assets/client2.svg";
+import client3 from "../../../assets/client3.svg";
+
+const OurClients = () => {
+  return (
+    <div>
+      <div className={classes.Container}>
+        <h2>Our Clients</h2>
+        <div className={classes.Section}>
+          <div className={classes.Content}>
+            <img src={client1} alt="" />
+          </div>{" "}
+          <div className={classes.Content}>
+            <img src={client2} alt="" />
+          </div>{" "}
+          <div className={classes.Content}>
+            <img src={client3} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default OurClients;
