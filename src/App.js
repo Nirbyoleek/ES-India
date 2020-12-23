7d506bc68851a046389ba9d997ae67925757fad1
Samaresh Samanta <ss9261@srmist.edu.in>
2020-12-23 14:18:46 +0530

    added the contact us page and implemented the Router in the app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import MainPage from "./components/LandingPage/mainPage/mainPage";
 import OurTeam from "./components/our team/ourTeam";
+import ContactUs from "./components/Contact Us/contactUs";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <OurTeam />
-      
-    </div>
+      <Router>
+        <Route exact path="/" component={MainPage} />  
+        <Route path="/Ourteam" component={OurTeam} /> 
+        <Route path="/ContactUs" component={ContactUs} /> 
+      </Router>
+      </div>
   );
 }
 
