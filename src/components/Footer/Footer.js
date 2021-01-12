ced57b03ee03a056563b5ea9e6ec5f2afc8be3d8
Nirbyoleek <nirbyoleek152001@gmail.com>
2021-01-12 20:06:10 +0530

    Fixed clients and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,34 +10,49 @@ const Footer = () => {
     <div>
       <div className={classes.Footer}>
         <div className={classes.Contact2}>
-          <p>
-            <MdEmail className={classes.Icons} /> solutionbyexpert@gmail.com
-          </p>
-          <p>
+          <div className={classes.AdContainer}>
+            <MdEmail className={classes.Icons} />
+            <div>solutionbyexpert@gmail.com</div>
+          </div>
+          <div className={classes.AdContainer}>
             <IoCall className={classes.Icons} />
-            +91-9810285722
-          </p>
-          <p>
-            <MdLocationOn className={classes.Icons} />
-            Noida Office: BG-06, Parsvnath Prestige Sector-93A, Noida, UP
-            PIN-201304 India.
-          </p>
-          <p>
-            <MdLocationOn className={classes.Icons} />
-            Ghaziabad Office: II-F-163 Nehru Nagar Ghaziabad, UP PIN-201001
-            India
-          </p>
+            <div>+91-9810285722</div>
+          </div>
+          <div>
+            <div className={classes.AdContainer}>
+              <div>
+                <MdLocationOn className={classes.Icons} />
+              </div>
+
+              <div className={classes.Address}>
+                Noida Office: BG-06, Parsvnath Prestige Sector-93A, Noida, UP
+                PIN-201304 India.
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className={classes.AdContainer}>
+              <div>
+                <MdLocationOn className={classes.Icons} />
+              </div>
+              <div className={classes.Address}>
+                Ghaziabad Office: II-F-163 Nehru Nagar Ghaziabad, UP PIN-201001
+                India
+              </div>
+            </div>
+          </div>
         </div>
         <div className={classes.Contact1}>
           <div className={classes.Content}>
             <img className={classes.Logo} src={Logo} alt="Logo" />
           </div>
           <div className={classes.Content}>
-            <p>
+            <div className={classes.Copyright1}>
               Copyright
-              <BiCopyright className={classes.Copyright} />
-              2020 Expert AE Solutions
-            </p>
+              <BiCopyright className={classes.Copy} /> 2021
+            </div>
+            <div className={classes.Copyright2}> Expert AE Solutions</div>
           </div>
         </div>
       </div>
