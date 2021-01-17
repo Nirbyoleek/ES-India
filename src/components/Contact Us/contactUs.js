bf20ee3a0cb9bae5c3dbd40779a34c92017a539e
Samaresh Samanta <ss9261@srmist.edu.in>
2021-01-17 12:52:58 +0530

    scroll to top arrow in all pages------DONE

--- src/components/Contact Us/contactUs.js
+++ src/components/Contact Us/contactUs.js
@@ -4,14 +4,23 @@ import Header from "../nav-header/nav contact";
 import Footer from "../Footer/Footer";
 import background from "../../assets/backgroundContactUs.svg";
 import { Helmet } from "react-helmet";
+import { FiArrowUp } from "react-icons/fi";
 
 const contactUs = () => {
   return (
-    <div className={classes.majorContainer} style={{backgroundImage: `url(${background})` ,}}>
+    <div id="top" className={classes.majorContainer} style={{backgroundImage: `url(${background})` ,}}>
       <Helmet>
         <title>Contact Us | Expert AE Solutions</title>
       </Helmet>
       <Header />
+      <div className={classes.TopButton}>
+        <a
+          href="#top"
+          style={{ textDecoration: "none", color: "#747474" }}
+        >
+          <FiArrowUp style={{ fontSize: "40px" }}></FiArrowUp>
+        </a>
+      </div>
       <center>
         <div class={classes.formContainer}>
           <h2>Contact Us</h2>
