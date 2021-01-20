2de3d4025a41e877b33a2b56d3f1f304e361a239
Samaresh Samanta <ss9261@srmist.edu.in>
2021-01-20 12:34:16 +0530

    form

--- src/components/Contact Us/contactUs.js
+++ src/components/Contact Us/contactUs.js
@@ -25,26 +25,6 @@ const contactUs = () => {
       </div>
       <center className={classes.Container}>
         <div class={classes.formContainer}>
-        <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
           <h2>Contact Us</h2>
           <hr color="black" className={classes.borderBelow} />
           <form name="contact" method="POST" data-netlify="true" netlify>
